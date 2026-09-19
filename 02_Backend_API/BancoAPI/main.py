9d754ff261239321ccb2cfd60529b396d1c416b6
bestwisheshadowhtml <bestwisheshadowhtml@gmail.com>
2026-09-19 15:38:06 -0300

    Update main.py

--- 02_Backend_API/BancoAPI/main.py
+++ 02_Backend_API/BancoAPI/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from database import obtener_conexion
 
 app = FastAPI()
 
@@ -8,3 +9,42 @@ def inicio():
     return {
         "mensaje": "API Banco funcionando"
     }
+
+
+@app.get("/clientes")
+def obtener_clientes():
+
+    conexion = obtener_conexion()
+
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        SELECT 
+            id_cliente,
+            nombre,
+            apellido,
+            rut,
+            telefono,
+            correo,
+            direccion
+        FROM Cliente
+    """)
+
+    clientes = []
+
+    for fila in cursor.fetchall():
+
+        clientes.append({
+            "id_cliente": fila.id_cliente,
+            "nombre": fila.nombre,
+            "apellido": fila.apellido,
+            "rut": fila.rut,
+            "telefono": fila.telefono,
+            "correo": fila.correo,
+            "direccion": fila.direccion
+        })
+
+
+    conexion.close()
+
+    return clientes
