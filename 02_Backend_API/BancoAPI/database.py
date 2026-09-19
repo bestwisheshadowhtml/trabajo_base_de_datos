5c7520337f1aeb4131c6cfaa4978311f64b3e24f
bestwisheshadowhtml <bestwisheshadowhtml@gmail.com>
2026-09-19 15:55:45 -0300

    Update database.py

--- 02_Backend_API/BancoAPI/database.py
+++ 02_Backend_API/BancoAPI/database.py
@@ -5,7 +5,7 @@ def obtener_conexion():
 
     conexion = pyodbc.connect(
         "DRIVER={SQL Server};"
-        "SERVER=DESKTOP-9M494AM\\SQLEXPRESS;"
+        "SERVER=DESKTOP-9M494AM\\SQLEXPRESS;" ## esta parte es el nombre de mi servidor de sql , es posbiel que cambie con respecto a cada computador este es el mio pero si es necesario cambiarlo se cambia por el de uno mismo , revisar en base de datos
         "DATABASE=BancoDB;"
         "Trusted_Connection=yes;"
     )
