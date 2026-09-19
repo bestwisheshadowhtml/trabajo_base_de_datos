a961aac735f0f83dd8374e989c13b8aa5288d448
bestwisheshadowhtml <bestwisheshadowhtml@gmail.com>
2026-09-19 15:38:22 -0300

    Update database.py

--- 02_Backend_API/BancoAPI/database.py
+++ 02_Backend_API/BancoAPI/database.py
@@ -1,1 +1,21 @@
-print("hola mundo")
+import pyodbc
+
+
+def obtener_conexion():
+
+    conexion = pyodbc.connect(
+        "DRIVER={SQL Server};"
+        "SERVER=DESKTOP-9M494AM\\SQLEXPRESS;"
+        "DATABASE=BancoDB;"
+        "Trusted_Connection=yes;"
+    )
+
+    return conexion
+
+
+try:
+    conexion = obtener_conexion()
+    print("Conexion exitosa a SQL Server")
+
+except Exception as error:
+    print("Error:", error)
